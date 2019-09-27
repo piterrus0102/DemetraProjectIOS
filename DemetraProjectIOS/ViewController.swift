c9977fb0b8bc6733b0b1499aa7ada6db7d3310d1
Khafizov Ruslan <piterrus@mail.ru>
2019-09-27 18:50:22 +0300

    Initial Commit

---
+++ DemetraProjectIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemetraProjectIOS
+//
+//  Created by Хафизов Руслан on 27/09/2019.
+//  Copyright © 2019 Хафизов Руслан. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
